8e8849f48c98026e9cb42930dfbe2240ed6ea7bb
Danilo Pianini <danilo.pianini@unibo.it>
2024-04-03 15:11:24 +0200

    preprocess the slides once before starting to react to the file system changes

--- serve.sh
+++ serve.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 trap "exit" INT TERM ERR
 trap "kill 0" EXIT
+shared-slides/preprocess.rb
 hugo server &
 while [ -e /proc/$! ]; do
     inotifywait -e modify,create,delete -r content shared-slides && sleep 1 && shared-slides/preprocess.rb;
